be69f06904623a8cbf01bad2c6e16f505b4fef02
qianrongli <qianrongli@uchain.one>
2018-10-26 11:00:10 +0800

    first commit

---
+++ src/main/java/com/kiss/kissnest/KissNestApplication.java
@@ -0,0 +1,16 @@
+package com.kiss.kissnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAutoConfiguration
+public class KissNestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KissNestApplication.class, args);
+    }
+}
---
+++ src/main/java/com/kiss/kissnest/mapper/GroupsMapper.java
@@ -0,0 +1,4 @@
+package com.kiss.kissnest.mapper;
+
+public class GroupsMapper {
+}
