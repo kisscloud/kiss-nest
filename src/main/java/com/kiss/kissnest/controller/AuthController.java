16503cc588dfdc5c737d101aad88437619142e57
qianrongli <qianrongli@uchain.one>
2018-10-29 20:57:32 +0800

    modify auth login input parameter

--- src/main/java/com/kiss/kissnest/controller/AuthController.java
+++ src/main/java/com/kiss/kissnest/controller/AuthController.java
@@ -2,7 +2,9 @@ package com.kiss.kissnest.controller;
 
 import com.kiss.account.input.ClientAuthorizationInput;
 import com.kiss.kissnest.feign.ClientServiceFeign;
+import com.kiss.kissnest.input.LoginInput;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +16,24 @@ public class AuthController {
     @Autowired
     private ClientServiceFeign clientServiceFeign;
 
+    @Value("${kiss.nest.clientId}")
+    private String clientId;
+
+    @Value("${kiss.nest.clientSecret}")
+    private String clientSecret;
+
+    @Value("${kiss.nest.clientExpired}")
+    private String clientExpired;
+
     @PostMapping("/code/login")
-    public ResultOutput ClientAuthorization(@RequestBody ClientAuthorizationInput clientAuthorizationInput) {
+    public ResultOutput ClientAuthorization(@RequestBody LoginInput loginInput) {
+
+        ClientAuthorizationInput clientAuthorizationInput = new ClientAuthorizationInput();
+        clientAuthorizationInput.setCode(loginInput.getAuthorizationCode());
+        clientAuthorizationInput.setClientId(clientId);
+        clientAuthorizationInput.setSecret(clientSecret);
+        clientAuthorizationInput.setExpired(Long.valueOf(clientExpired));
+
         return clientServiceFeign.ClientAuthorization(clientAuthorizationInput);
     }
 }
---
+++ src/main/java/com/kiss/kissnest/input/LoginInput.java
@@ -0,0 +1,9 @@
+package com.kiss.kissnest.input;
+
+import lombok.Data;
+
+@Data
+public class LoginInput {
+
+    private String authorizationCode;
+}
