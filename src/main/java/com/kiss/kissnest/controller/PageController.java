1520bfeb44b0189ff08ab302dfb800c822f277cd
koyeo <im@koyeo.io>
2018-11-05 16:15:06 +0800

    edit page controller

---
+++ src/main/java/com/kiss/kissnest/controller/PageController.java
@@ -0,0 +1,54 @@
+package com.kiss.kissnest.controller;
+
+import com.kiss.kissnest.service.GroupService;
+import com.kiss.kissnest.service.ProjectService;
+import com.kiss.kissnest.util.ResultOutputUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import output.ResultOutput;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@Api(tags = "Page", description = "控制台页面接口")
+@RequestMapping("/page")
+public class PageController {
+
+    @Autowired
+    private GroupService groupService;
+
+    @Autowired
+    private ProjectService projectService;
+
+    @ApiOperation(value = "获取项目组页面参数")
+    @GetMapping("/groups")
+    public ResultOutput GetPageGroupsParams() {
+
+        ResultOutput groups = groupService.getGroups();
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("groups", groups.getData());
+        result.put("status", groups.getData());
+
+        return ResultOutputUtil.success(result);
+    }
+
+    @ApiOperation(value = "获取项目组页面参数")
+    @GetMapping("/projects")
+    public ResultOutput GetPageProjectsParams() {
+
+        ResultOutput projects = projectService.getProjects();
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("projects", projects.getData());
+        result.put("status", projects.getData());
+
+        return ResultOutputUtil.success(result);
+    }
+
+}
