81ad29306b11e46489537faf1eb822c876fd4bfa
koyeo <im@koyeo.io>
2018-11-16 11:59:34 +0800

    edit api

--- src/main/java/com/kiss/kissnest/config/Swagger2Config.java
+++ src/main/java/com/kiss/kissnest/config/Swagger2Config.java
@@ -29,8 +29,7 @@ public class Swagger2Config {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Kiss 账户服务")
-                .description("提供了账户验证、权限控制等功能")
+                .title("Nest - 开发管理系统")
                 .termsOfServiceUrl("https://kisscloud.io")
                 .version("1.0")
                 .build();
