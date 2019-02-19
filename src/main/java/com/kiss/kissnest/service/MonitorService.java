986624ac9349997aba6d5e364cd6976ee22a8e67
koyeo <im@koyeo.io>
2019-02-19 14:46:52 +0800

    添加缓存

--- src/main/java/com/kiss/kissnest/service/MonitorService.java
+++ src/main/java/com/kiss/kissnest/service/MonitorService.java
@@ -1,7 +1,19 @@
 package com.kiss.kissnest.service;
 
+import com.kiss.kissnest.input.MonitorServerInput;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 @Service
 public class MonitorService {
+
+    @Resource
+    public RedisTemplate redisTemplate;
+
+    public void cacheServerMonitorData(MonitorServerInput monitorServerInput) {
+
+    }
+
 }
