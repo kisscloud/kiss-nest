e5628be36ff23beb0281e60b45a396b205964227
koyeo <im@koyeo.io>
2019-03-21 23:08:56 +0800

    test

--- src/main/java/com/kiss/kissnest/input/JenkinsNotificationBuild.java
+++ src/main/java/com/kiss/kissnest/input/JenkinsNotificationBuild.java
@@ -14,4 +14,8 @@ public class JenkinsNotificationBuild {
     private JenkinsNotificationSCM scm;
     private String notes;
     private String log;
+
+    public String getLog() {
+        return log == null ? "" : log;
+    }
 }
