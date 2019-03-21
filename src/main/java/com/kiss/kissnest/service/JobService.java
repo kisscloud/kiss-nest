a12dddf5a1e31c7253e741bb290f658ea5f41c68
koyeo <im@koyeo.io>
2019-03-21 23:59:54 +0800

    test

--- src/main/java/com/kiss/kissnest/service/JobService.java
+++ src/main/java/com/kiss/kissnest/service/JobService.java
@@ -860,8 +860,7 @@ public class JobService {
             buildLog.setLogUrl(jobUrl + "console");
             buildLog.setStatus(BuildJobStatusEnums.QUEUEING.value());
             buildLog.setNumber(number);
-//            buildLog.setBranch(branch);
-//            buildLog.setVersion(version);
+            buildLog.setVersion(version);
             buildLogDao.updateBuildLog(buildLog);
 
             BuildLogOutput buildLogOutput = outputUtil.toBuildLogOutput(buildLog);
